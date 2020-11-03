333ef0b510034dc4bdeec9737c1beac1bd4122fc
Carlos Capitán <carcapagu@gmail.com>
2020-11-03 12:04:06 +0100

    Added files of the previous functional version

---
+++ app/custom-modeler/custom/Types.js
@@ -0,0 +1,112 @@
+export const label = [
+    'custom:Resource',
+    'custom:ResourceAbsence',
+    'custom:Role',
+    'custom:RoleAbsence',
+    'custom:Group',
+    'custom:GroupAbsence',
+    'custom:Clock',
+    'custom:TimeSlot',
+    'custom:nyanCat',
+    'custom:Person',
+    'custom:RoleRALph',
+    'custom:Personcap',
+    'custom:Orgunit',
+    'custom:Position'
+]
+
+export const externalLabel = [
+    'custom:Resource',
+    'custom:ResourceAbsence',
+    'custom:Role',
+    'custom:RoleAbsence',
+    'custom:Group',
+    'custom:GroupAbsence',
+    'custom:Clock',
+    'custom:nyanCat',
+    'custom:Person',
+    'custom:RoleRALph',
+    //'custom:Personcap'
+    
+    
+]
+
+export const connections = [
+    'custom:ResourceArc',
+    'custom:ConsequenceFlow',
+    'custom:TimeDistanceArcStart',
+    'custom:TimeDistanceArcEnd'
+]
+
+export const directEdit = [
+    // 'custom:Resource',
+    // 'custom:Role',
+    // 'custom:Group',
+    'custom:Clock',
+    'custom:TimeSlot',
+    'custom:nyanCat',
+    'custom:Person',
+    'custom:RoleRALph',
+    'custom:Personcap',
+    'custom:Orgunit',
+    'custom:Position'
+]
+
+export const resourceArcElements = [
+    'custom:Clock',
+    'custom:Resource',
+    'custom:ResourceAbsence',
+    'custom:Role',
+    'custom:RoleAbsence',
+    'custom:Group',
+    'custom:GroupAbsence',
+    'custom:TimeSlot',
+    'custom:nyanCat',
+    'custom:Person',
+    'custom:RoleRALph',
+    'custom:Personcap',
+    'custom:Orgunit',
+    'custom:Position'
+]
+
+export const custom = [
+    'custom:Clock',
+    'custom:Resource',
+    'custom:ResourceAbsence',
+    'custom:Role',
+    'custom:RoleAbsence',
+    'custom:Group',
+    'custom:GroupAbsence',
+    'custom:TimeSlot',
+    'custom:ResourceArc',
+    'custom:ConsequenceFlow',
+    'custom:TimeDistanceArcStart',
+    'custom:TimeDistanceArcEnd',
+    'custom:nyanCat',
+    'custom:Person',
+    'custom:RoleRALph',
+    'custom:Personcap',
+    'custom:Orgunit',
+    'custom:Position'
+]
+
+export function isCustomShape(type) {
+    if (typeof type === 'object')
+        type = type.type
+
+    return type.includes('custom:') && !connections.includes(type)
+}
+
+export function isCustomConnection(type) {
+    if (typeof type === 'object') {
+        type = type.type
+    }
+    return type.includes('custom:') && connections.includes(type)
+}
+
+export function isCustomResourceArcElement(type) {
+    if (typeof type === 'object') {
+        type = type.type
+    }
+    return resourceArcElements.includes(type)
+}
