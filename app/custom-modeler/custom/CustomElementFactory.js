a2b4363b558f89a680683922c69e2a6b48d58740
Carlos Capitán <carcapagu@gmail.com>
2020-11-17 12:51:41 +0100

    Fixed hierarchy connectors resolution

--- app/custom-modeler/custom/CustomElementFactory.js
+++ app/custom-modeler/custom/CustomElementFactory.js
@@ -170,7 +170,9 @@ CustomElementFactory.prototype._getCustomElementSize = function(type) {
     'custom:Position':{width:75,height:75},
     'custom:History-Same':{width:75,height:75},
     'custom:History-Any':{width:75,height:75},
-    'custom:delegateTo':{width:55,height:55}
+    'custom:delegateTo':{width:82,height:43},
+    'custom:reportsTo':{width:58,height:43}
+
   };
 
   return shapes[type] || shapes.__default;
